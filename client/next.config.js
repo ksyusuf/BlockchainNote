9425aa642723cd102a877a253d3f1e4b640c5459
ksyusuf <ksyusuf85@gmail.com>
2025-06-01 21:39:19 +0300

    to vercel

--- client/next.config.js
+++ client/next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true
+  swcMinify: true,
+  experimental: {
+    serverComponentsExternalPackages: ['@stellar/stellar-sdk']
+  }
 };
 
 module.exports = nextConfig;
