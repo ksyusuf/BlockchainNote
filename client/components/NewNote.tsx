55966c613e1d0bcadf598d3d0d260153c7fa9db3
ksyusuf <ksyusuf85@gmail.com>
2025-06-04 03:11:59 +0300

    minicik düzeltme

--- client/components/NewNote.tsx
+++ client/components/NewNote.tsx
@@ -90,7 +90,7 @@ export default function NewNote({ publicKey, onNoteCreated, showForm, onClose }:
                   <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
                   <path className="opacity-75" fill="currentColor" d="m4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                 </svg>
-                {createNoteTransactionId ? "Bekleyin..." : "İşlemi Onayı..."}
+                {createNoteTransactionId ? "Bekleyin..." : "İşlem Onayı..."}
               </>
             ) : (
               "Kaydet"
